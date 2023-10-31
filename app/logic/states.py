04a30c0661e38ba91c5c53e0efcbd7136abdf326
green-dino <104174275+green-dino@users.noreply.github.com>
2023-10-31 15:04:15 -0700

    updates

--- app/logic/states.py
+++ app/logic/states.py
@@ -6,6 +6,16 @@ N1 = 5  # Number of agents in group 1
 N2 = 3  # Number of agents in group 2
 N = N1 + N2  # Total number of agents
 
+# POMDP components
+T = 10  # Time horizon
+N = 100  # Maximum number of time steps (N ≤ ∞)
+gamma = 0.9  # Discount factor
+S = [f'S{i}' for i in range(1, 11)]  # State space
+A = [f'A{i}' for i in range(1, 6)]  # Action space
+At = {t: [f'A{i}' for i in range(1, 6)] for t in range(1, T + 1)}  # Available actions at each time step
+O = [f'O{i}' for i in range(1, 6)]  # Observation space
+
+
 # Define state space, actions, decisions, and observations
 state_space = [f'X{i}' for i in range(1, N1 + 1)]
 action_space = [f'A{i}' for i in range(1, N1 + 1)]
@@ -15,41 +25,39 @@ observation_space = [f'Y{i}' for i in range(1, N1 + 1)]
 # Define transition probabilities Qn for each agent in group 1
 transition_probabilities = {f'Q{i}': [[0.7, 0.3], [0.4, 0.6]] for i in range(1, N1 + 1)}
 
-# Define agent utility functions for group 1
-def utility_group1(A, D):
-    utility = {}
-    Wr = 0.5  # Weight for security
-    Wc = 0.5  # Weight for efficiency
-    u_lower = -10  # Lower bound for utility
-    u_upper = 10  # Upper bound for utility
-    epsilon = 0.2  # Maximum utility error
+def calculate_utility(r, c, Wr, Wc, u_lower, u_upper, epsilon):
+    # Calculate utility with given weights
+    u = Wr * r - Wc * c
 
+    # Apply utility bounds and potential utility errors
+    u += random.uniform(-epsilon, epsilon)  # Add utility error
+
+    # Ensure the utility is within defined bounds
+    u = max(u_lower, min(u_upper, u))
+
+    return u
+
+# Define agent utility functions for group 1
+def utility_group1(A, D, Wr, Wc, u_lower, u_upper, epsilon):
+    utility = {}
     for n in range(1, N1 + 1):
         An = A[f'A{n}']
         # Simulate observable feedback (effectiveness and cost)
         r = effectiveness(D, An)  # Calculate effectiveness
-        c = cost(D)  # Calculate cost
+        c = cost(An)  # Calculate cost
 
-        # Calculate utility with given weights
-        u = Wr * r - Wc * c
-
-        # Apply utility bounds and potential utility errors
-        u += random.uniform(-epsilon, epsilon)  # Add utility error
-
-        # Ensure the utility is within defined bounds
-        u = max(u_lower, min(u_upper, u))
-
-        utility[f'U{n}'] = u
+        # Calculate utility
+        utility[f'U{n}'] = calculate_utility(r, c, Wr, Wc, u_lower, u_upper, epsilon)
 
     return utility
 
 # Define effectiveness and cost functions
 def effectiveness(D, A):
-    # Implement your effectiveness function (e.g., number of failed attack tries)
+    # Implement your effectiveness function (e.g., based on the CIA triad)
     return random.uniform(0, 1)
 
-def cost(D):
-    # Implement your cost function (e.g., overhead induced by defense actions)
+def cost(A):
+    # Implement your cost function (e.g., resources required by actions)
     return random.uniform(0, 1)
 
 # Define action sets for players
