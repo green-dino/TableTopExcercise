2ddd2dce77e24def57f7b2934adeee2faf166273
green-dino <104174275+green-dino@users.noreply.github.com>
2023-10-31 15:15:31 -0700

    updated mtd

--- app/logic/mtd.py
+++ app/logic/mtd.py
@@ -13,7 +13,32 @@ RWK = {("Weakness1", "KnowledgeBlock1"), ("Weakness2", "KnowledgeBlock2"), ("Wea
 # Define relationships between knowledge blocks and MTD techniques
 RKM = {("KnowledgeBlock1", "MTDTechnique1"), ("KnowledgeBlock2", "MTDTechnique2"), ("KnowledgeBlock3", "MTDTechnique3")}
 
-# Print the 7-tuple
+# Attacker Strategies
+attacker_strategies = {
+    "Uniform-Uncompromised": "Selects uniformly among those servers under the defender's control.",
+    "MaxProbe-Uncompromised": "Selects the server that has been probed the most since last reimage (that the attacker knows about), among those servers under the defender's control, breaking ties uniformly.",
+    "Control-Threshold": "If the attacker controls less than a threshold fraction of the servers, it chooses to probe the server that has been probed the most since last reimage (as far as it is aware) among those it does not currently control. Ties are broken uniformly among all eligible servers. A minimum waiting period of one time unit separates any two consecutive actions."
+}
+
+# Defender Strategies
+defender_strategies = {
+    "Uniform": "Selects uniformly among all up servers.",
+    "MaxProbe": "Selects the server that has been probed most since its last reimage, breaking ties uniformly.",
+    "ProbeCount-or-Period": "Reimages a server whenever it detects a threshold number of probes since the last reimage or if it has been probed at least once but not within the specified period.",
+    "Control-Threshold": "Reimages a server when the fraction of servers controlled falls below a threshold.",
+    "Control-Target": "Similar to Control-Threshold, but based on a target rather than a threshold.",
+
+}
+
+# Equiliberia 
+equilibira_concepts = {
+    "MaxDef": "Aggressively reimages servers, making it futile for the attacker to compromise servers.",
+    "MaxAtt": "Attacker probes aggressively, and defender reimages infrequently or ineffectively.",
+    "Share": "Moderate attack and defense levels where both players achieve their objectives.",
+    "Fight": "Robust attack and defense activity, resulting in persistent contention with neither player achieving their objectives to a satisfactory degree."
+}
+
+# Print the 7-tuple, Attacker Strategies, and Defender Strategies
 print("7-tuple (S, RSW, W, RWK, K, RKM, M):")
 print("S (Services):", S)
 print("RSW (Relationships between Services and Weaknesses):", RSW)
@@ -22,3 +47,11 @@ print("RWK (Relationships between Weaknesses and Knowledge Blocks):", RWK)
 print("K (Knowledge Blocks):", K)
 print("RKM (Relationships between Knowledge Blocks and MTD Techniques):", RKM)
 print("M (MTD Techniques):", M)
+
+print("\nAttacker Strategies:")
+for strategy, description in attacker_strategies.items():
+    print(f"{strategy}: {description}")
+
+print("\nDefender Strategies:")
+for strategy, description in defender_strategies.items():
+    print(f"{strategy}: {description}")
